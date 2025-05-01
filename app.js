236ee2c6492d1d9a0348417c683543f977fb87e8
sahil <sahilvishwakarma6260@gmail.com>
2025-05-01 15:05:39 +0530

    added some routes

--- app.js
+++ app.js
@@ -1,8 +1,26 @@
 const express = require('express');
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("hello");
+
+app.get("/admin/:name/:age", (req, res) => {
+  res.send({name:req.params.name,age:req.params.age,comment:req.query.comment});
+});
+
+app.get("/user", (req, res) => {
+  res.send({name:"admin",age:23});
+});
+
+app.post("/user", (req, res) => {
+//   res.send(`user posted a comment : ${req.body.comment}`);  //TODO
+  res.send(`user posted a comment.`);
+});
+
+app.delete("/user", (req, res) => {
+  res.send("deleted");
+});
+
+app.use("/", (req, res) => {
+  res.send("hello use");
 });
 
 app.listen(6700, () => {
