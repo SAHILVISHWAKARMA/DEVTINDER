1e1d0966c84fc1140cdc7f7ee7f81233f3d465cb
sahil <sahilvishwakarma6260@gmail.com>
2025-05-06 15:12:39 +0530

    perform crup operation

--- app.js
+++ app.js
@@ -3,29 +3,86 @@ const connectDB = require('./src/config/database');
 const User = require('./src/models/user');
 const app = express();
 
-app.post('/signup',async (req, res)=>{
+app.use(express.json());
+
+//get user by mail
+app.get('/user', async (req, res) => {
+  let { email } = req.body;
+  try {
+    // const user = await User.find({ email });
+    const user = await User.findOne({ email });
+    if (user) {
+      res.send({ message: "user found", user })
+    } else {
+      res.status(404).send({ comment: "user not found" })
+    }
+  } catch (err) {
+    res.status(400).send("something went wrong"+err)
+  }
+});
+
+//get all feed data 
+app.get('/feed', async (req, res)=>{
+  try{
+    let users = await User.find();
+    if(users){
+      res.send({message:"users found",users})
+    } else{
+      res.status(400).send({message:"user not found"})
+    }
+  } catch(err){
+    res.status(400).send("something went wrong"+err)
+  }
+  
+});
+
+//sign up the user
+app.post('/signup', async (req, res) => {
+  let { firstName, lastName, email, password, age, gender } = req.body;
   const user = new User({
-    firstName:"virat",
-    lastName:"kohli",
-    email:"virat@kohli123.com",
-    password:"virat@123",
-    age:34,
-    gender:"male"
+    firstName,
+    lastName,
+    email,
+    password,
+    age,
+    gender
   });
 
-  try{
+  try {
     await user.save();
     res.send("user added successfully!!!");
-  } catch(err){
-    res.status(400).send("unable to save user :",err.message)
+  } catch (err) {
+    res.status(400).send("unable to save user :", err.message)
   }
-})
+});
 
-connectDB().then((res)=>{
-  app.listen(6700, () => {  
+//delete user by id
+app.delete('/user', async (req, res) =>{
+  try{
+    let {id} = req.body;
+    let user = await User.findByIdAndDelete(id);
+    res.send({message:"user deleted", user})
+  } catch (err) {
+    res.status(400).send("unable to save user :", err.message);
+  }
+});
+
+// update the user
+app.patch('/user', async (req, res) =>{
+  try{
+    let data = req.body;
+    let user = await User.findByIdAndUpdate(data.id,data,{returnDocument:"after"});
+    res.send({message:"user updated", user})
+  } catch (err) {
+    res.status(400).send("unable to save user :", err.message);
+  }
+});
+
+connectDB().then((res) => {
+  app.listen(6700, () => {
     console.log(`server listening on port 6700`);
   });
-}).catch((err)=>{
-  console.log("failed to connect db ",err);
-})
+}).catch((err) => {
+  console.log("failed to connect db ", err);
+});
 
--- src/models/user.js
+++ src/models/user.js
@@ -1,26 +1,47 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema(
-    {
-        firstName:{
-            type: String
-        },
-        lastName:{
-            type: String
-        },
-        age:{
-            type: Number
-        },
-        gender:{
-            type: String
-        },
-        email:{
-            type: String
-        },
-        password:{
-            type: String
-        },
-    }
+  {
+    firstName: {
+      type: String,
+      required: true,
+      trim: true,
+      minlength: 2,
+    },
+    lastName: {
+      type: String,
+      required: true,
+      trim: true,
+      minlength: 2,
+    },
+    age: {
+      type: Number,
+      min: 0,
+      max: 120,
+    },
+    gender: {
+      type: String,
+      enum: ['Male', 'Female', 'Other'],
+      default: 'Other',
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+      match: [/^\S+@\S+\.\S+$/, 'Please enter a valid email'],
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: 6,
+      select: false, // exclude from queries unless explicitly selected
+    },
+  },
+  {
+    timestamps: true, // adds createdAt and updatedAt
+  }
 );
 
-module.exports = mongoose.model('user',userSchema)
+module.exports = mongoose.model('User', userSchema);
